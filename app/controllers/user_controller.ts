14b55cfd90564739a2d7523b7a12989fbf4b37ff
Siddharth <siddharth.bharti@devslane.com>
2024-10-24 11:20:35 +0530

    WIP

--- app/controllers/user_controller.ts
+++ app/controllers/user_controller.ts
@@ -1,4 +1,6 @@
-import type { HttpContext } from '@adonisjs/core/http'
+import User from '#models/user'
+import { HttpContext } from '@adonisjs/core/http'
+import db from '@adonisjs/lucid/services/db'
 
 export default class UserController {
   async me({ auth }: HttpContext) {
@@ -7,5 +9,16 @@ export default class UserController {
     return {
       data: { user: auth.user },
     }
+  }
+
+  async list({ request }: HttpContext) {
+    const page = request.input('page', 1)
+    const limit = 10
+
+    // const users = await db.from('users').paginate(page, limit)
+
+    const users = await User.all()
+
+    return { data: { users } }
   }
 }
--- app/modules/user_routes.ts
+++ app/modules/user_routes.ts
@@ -3,6 +3,10 @@ import router from '@adonisjs/core/services/router'
 
 router
   .group(() => {
-    router.get('/', [UserController, 'me'])
+    router.get('/', [UserController, 'me']), router.get('/users', [UserController, 'list'])
   })
   .prefix('/me')
+
+router.group(() => {
+  router.get('/users', [UserController, 'list'])
+})
